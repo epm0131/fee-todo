36f9bb9b8484cfb76fb14325eb4f0bec69d917ca
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-27 16:33:10 -0400

    I added a document query/event so that when i press enter i get back a log

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,17 @@
 (function(){
+  document
+  .querySelector('form')
+  .addEventListener('submit', function storeSubmitItem(event) {
+    event.preventDefault();
+    console.log('hello!');
+
+
+
+  });
+
+
+
+
 
 
 
