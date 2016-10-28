9115ee3c15a5de1c1d0bdfeb184d8fc4987daf4f
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-28 14:10:50 -0400

    I add .value to my pressKey and have started setting up my variables for my toDoList

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,16 @@
 (function(){
+
+
   document
   .querySelector('form')
-  .addEventListener('submit', function storeSubmitItem(event) {
+  .addEventListener('submit', function pressKey(event) {
     event.preventDefault();
-    console.log('hello!');
+    console.log(event);
+    console.log( document.querySelector('.new-todo').value );
 
+    var listItem = document.createElement('li');
+    var articleItem = document.createElement('article')
+    var buttonCheck = document.createElement('button')
 
 
   });
