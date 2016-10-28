307fddd076d47f0a21f1c7072ff56dbb2a754a50
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-28 14:20:52 -0400

    I created my vars for the li to mimic the assignment

--- js/todo.js
+++ js/todo.js
@@ -8,10 +8,23 @@
     console.log(event);
     console.log( document.querySelector('.new-todo').value );
 
-    var listItem = document.createElement('li');
-    var articleItem = document.createElement('article')
-    var buttonCheck = document.createElement('button')
+function toDoList(text) {
 
+    var listItem = document.createElement('li');
+
+    var articleItem = document.createElement('article');
+
+    var buttonCheck = document.createElement('button');
+    buttonCheck.classList.add('check');
+    articleItem.appendChild(buttonCheck);
+
+    var pItem = document.createElement('p');
+    pItem.innerText = text;
+
+    var buttonDelete = document.createElement('xButton');
+    buttonDelete.classList.add('delete');
+    articleItem.appendChild(buttonDelete);
+}
 
   });
 
