c46b84b8e41fc0fbf0bd30b5cc9557190aac5a65
Ettore Merendino <merendino.ettore@gmail.com>
2017-02-24 12:43:36 -0500

    refactored

---
+++ js/data.js
@@ -0,0 +1,32 @@
+
+(function() {
+  'use strict';
+
+  window.todo = window.todo || {};
+  window.todo.todoData = todoData;
+  window.todo.updateCounter = updateCounter;
+  window.todo.totalCount = totalCount;
+
+  var numberOfTasksLeft = 0;
+  var todoArray = [];
+
+  function todoData(newTask) {
+    numberOfTasksLeft++;
+    var data = {
+      text: newTask,
+      time: Date.now()
+    };
+    todoArray.push(data);
+  }
+
+  function updateCounter(number) {
+    if(number === 1) {
+      numberOfTasksLeft++;
+    } else {
+      numberOfTasksLeft--;
+    }
+  }
+  function totalCount() {
+    return numberOfTasksLeft;
+  }
+})();
---
+++ js/ui.js
@@ -0,0 +1,66 @@
+(function() {
+  'use strict';
+
+  window.todo = window.todo || {};
+  window.todo.todoApp = todoApp;
+
+  /**
+  * Creates a new task in the to do list
+  * @param  {String} items new task
+  */
+  function todoApp(items) {
+      var list = document.createElement('li');
+      var article = document.createElement('article');
+      var check = document.createElement('button');
+      check.classList.add('check');
+      article.appendChild(check);
+      var par = document.createElement('p');
+      par.innerText = items;
+      article.appendChild(par);
+      var del = document.createElement('button');
+      del.classList.add('delete');
+      del.innerText = 'X';
+      article.appendChild(del);
+      list.appendChild(article);
+      console.log('what am i?', list);
+
+      document
+        .querySelector('ul.items')
+        .appendChild(list);
+  }
+
+  document
+    .querySelector('form')
+    .addEventListener('submit', function handleEnter(eventobj){
+        eventobj.preventDefault();
+        var input = document.querySelector('.new-todo');
+        todoApp(input.value);
+        window.todo.todoData(input.value);
+
+        updateNumofTasksLeft();
+    });
+
+  document
+    .querySelector('ul.items')
+    .addEventListener('click', function completed (eventObj) {
+        if(eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'check') {
+          if(eventObj.target.parentNode.className === 'completed') {
+            eventObj.target.parentNode.classList.remove('completed');
+            window.todo.updateCounter(1);
+          } else {
+              eventObj.target.parentNode.classList.add('completed');
+              window.todo.updateCounter();
+          }
+          updateNumofTasksLeft();
+        } else if
+        (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete') {
+          eventObj.target.parentNode.parentNode.remove();
+          window.todo.updateCounter();
+        }
+        updateNumofTasksLeft();
+    });
+
+  function updateNumofTasksLeft() {
+    document.querySelector('.incomplete-items').innerText = window.todo.totalCount();
+  }
+})();
